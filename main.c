052954ad47293ac2d1ab20980f9e77489f73ce67
IsShai <45069507+IsShai@users.noreply.github.com>
2019-10-06 01:24:22 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,5 @@
 #include "types.h"
-/*-------------------------------
-Final Project
-Name: Lior Ben Yehuda 305346660
-Name: Shai Hassan 200175131
-Date: 18.06.2017
--------------------------------*/
+
 
 int main(int argc, char *argv[])
 {
